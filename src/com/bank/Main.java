c1eaf80a71b6117cace2d7745347d3f7d5cbbe8f
unknown <79323700+leonardosantino@users.noreply.github.com>
2022-01-07 18:53:14 -0300

    add project

---
+++ src/com/bank/Main.java
@@ -0,0 +1,20 @@
+package com.bank;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Cliente venilton = new Cliente();
+        venilton.setNome("Venilton");
+
+        Conta cc = new ContaCorrente(venilton);
+        Conta poupanca = new ContaPoupanca(venilton);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+
+    }
+}
